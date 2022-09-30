4d53dcfbc14738e2e235166aec0fb728aa74949f
Malte Linde Neveling <malte.linde.neveling@gmail.com>
2022-09-30 20:42:59 +0200

    Orbit camera

--- Assets/WormMovement.cs
+++ Assets/WormMovement.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WormScript : MonoBehaviour
+public class WormMovement : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
+    // private Vector3 playerVelocity;
+    // private bool groundedPlayer;
     private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    // private float jumpHeight = 1.0f;
+    // private float gravityValue = -9.81f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +20,9 @@ public class WormScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         var move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        controller.Move(move * playerSpeed);
+        controller.Move(move * playerSpeed * Time.deltaTime);
     }
 }
